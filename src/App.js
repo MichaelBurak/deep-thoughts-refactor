c6f5fbf4835b3538368e1deeb7192e09a96bcac5
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2020-12-09 18:23:24 -0700

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <h1> Deep Thoughts Quotes: Take 2 </h1>
+  <Button color="primary">Now with Material UI!</Button>
     </div>
-  );
+  )
 }
 
 export default App;
